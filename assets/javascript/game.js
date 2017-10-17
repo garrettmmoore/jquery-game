6c9aa951569026b7b19de926889a44cabb8683ce
Garrett Moore <garrettmarkmoore@gmail.com>
2017-10-16 23:09:37 -0700

    Remove unused reset.css. Clean up style.css file. Clean up game.js. Clean up index.html file.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,6 @@
-$(document).ready(function() 
-{	
+$(document).ready(function () {
 
-
-// Declare Variables ------------------------------------------------------------>
-
+	// Declare Variables ------------------------------------------------------------>
 
 	var winningNumber = 0;
 	var totalScore = 0;
@@ -14,12 +11,10 @@ $(document).ready(function()
 	var topazRandom = 0;
 	var emeraldRandom = 0;
 
-
-// Randomizer functions ------------------------------------------------------------>
-
+	// Randomizer functions ------------------------------------------------------------>
 
 	// Generate random Winning number 
-	function newWinningNumber(){
+	function newWinningNumber() {
 		winningNumber = Math.floor(Math.random() * 119) + 1;
 		console.log("Winning Number: " + winningNumber);
 		// Print winning number to the random-number-box
@@ -28,38 +23,35 @@ $(document).ready(function()
 		gamePlay(winningNumber);
 		return winningNumber;
 	}
-	
 
 	// Generate random values for each Gem
 	// Assign the gems to their appropriate random value
-	function newRubyRandom(){
-	rubyRandom = Math.floor(Math.random() * 11) + 1;
-	console.log("Current ruby Value: " + rubyRandom);
-	$("#ruby").val(rubyRandom);
-	$("#ruby").val();
+	function newRubyRandom() {
+		rubyRandom = Math.floor(Math.random() * 11) + 1;
+		console.log("Current ruby Value: " + rubyRandom);
+		$("#ruby").val(rubyRandom);
+		$("#ruby").val();
 	}
 
-	function newSapphireRandom(){
-	sapphireRandom = Math.floor(Math.random() * 11) + 1;
-	console.log("Current sapphire Value: " + sapphireRandom);
-	$("#sapphire").val(sapphireRandom);
+	function newSapphireRandom() {
+		sapphireRandom = Math.floor(Math.random() * 11) + 1;
+		console.log("Current sapphire Value: " + sapphireRandom);
+		$("#sapphire").val(sapphireRandom);
 	}
 
-	function newTopazRandom(){
-	topazRandom = Math.floor(Math.random() * 11) + 1;
-	console.log("Current topaz Value: " + topazRandom);
-	$("#topaz").val(topazRandom);
+	function newTopazRandom() {
+		topazRandom = Math.floor(Math.random() * 11) + 1;
+		console.log("Current topaz Value: " + topazRandom);
+		$("#topaz").val(topazRandom);
 	}
 
-	function newEmeraldRandom(){
-	emeraldRandom = Math.floor(Math.random() * 11) + 1;
-	console.log("Current emerald Value: " + emeraldRandom);
-	$("#emerald").val(emeraldRandom);
+	function newEmeraldRandom() {
+		emeraldRandom = Math.floor(Math.random() * 11) + 1;
+		console.log("Current emerald Value: " + emeraldRandom);
+		$("#emerald").val(emeraldRandom);
 	}
 
-
-// Click Functions --------------------------------------------------------------->
-
+	// Click Functions --------------------------------------------------------------->
 
 	newWinningNumber();
 	newRubyRandom();
@@ -67,63 +59,54 @@ $(document).ready(function()
 	newTopazRandom();
 	newEmeraldRandom();
 
+	// Game Play Function ------------------------------------------------------------>
 
-// Game Play Function ------------------------------------------------------------>
-
-
-	function gamePlay()
-	{	
-		if(totalScore == winningNumber)
-		{
+	function gamePlay() {
+		if (totalScore == winningNumber) {
 			wins++;
 			$(".result").html("You win!!");
 			reset();
 		}
 		console.log(totalScore);
 		console.log(winningNumber);
-		if (totalScore > winningNumber)
-		{
+		if (totalScore > winningNumber) {
 			losses++;
 			$(".result").html("You lose..");
 			reset();
 		}
-
 	}
 
+	// Click Functions ------------------------------------------------------------>
 
-// Click Functions ------------------------------------------------------------>
-
-function clickHelper(gem){
+	function clickHelper(gem) {
 		$(".result").html("");
 		console.log(rubyRandom);
 		$(".gem-value-box").append(totalScore = totalScore + gem);
 		$(".gem-value-box").append(" | ");
 		console.log(totalScore);
 		gamePlay();
-}
+	}
 	// When Ruby is clicked, add value to totalScore on each click
-	$("#ruby").on("click", function ruby(){
+	$("#ruby").on("click", function ruby() {
 		clickHelper(rubyRandom);
 	});
 	// When Sapphire is clicked, add value to totalScore on each click
-	
-	$("#sapphire").on("click", function sapphire(){
+
+	$("#sapphire").on("click", function sapphire() {
 		clickHelper(sapphireRandom);
-	});	
+	});
 	// When Topaz is clicked, add value to totalScore on each click
-	$("#topaz").on("click", function topaz(){
+	$("#topaz").on("click", function topaz() {
 		clickHelper(topazRandom);
 	});
 	// When Emerald is clicked, add value to totalScore on each click
-	$("#emerald").on("click", function emerald(){
+	$("#emerald").on("click", function emerald() {
 		clickHelper(emeraldRandom);
 	});
 
+	// Reset Game after win or loss --------------------------------------------------->
 
-// Reset Game after win or loss --------------------------------------------------->
-
-
-	function reset(){
+	function reset() {
 
 		// Reset score to zero
 		totalScore = 0;
@@ -143,14 +126,10 @@ function clickHelper(gem){
 		newSapphireRandom();
 		newTopazRandom();
 		newEmeraldRandom();
-
 		console.log(wins);
-
 	}
 
 	// console.log(totalScore[0] + totalScore[1]);
 	console.log(totalScore);
 	console.log(wins);
-
 });
-
